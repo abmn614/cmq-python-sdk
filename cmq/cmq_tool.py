28e7b824ce25e00fbea56da98321c42dc6ebfd53
limingding <limingding>
2021-07-23 16:20:06 +0800

    modify log path

--- cmq/cmq_tool.py
+++ cmq/cmq_tool.py
@@ -12,6 +12,11 @@ from cmq.cmq_exception import *
 METHODS = ["POST", "GET"]
 PERMISSION_ACTIONS = ["setqueueattributes", "getqueueattributes", "sendmessage", "receivemessage", "deletemessage", "peekmessage", "changevisibility"]
 
+root_path = sys.path[0]
+log_dir_base = root_path + '/logs/'
+if not os.path.exists(log_dir_base):
+   os.makedirs(log_dir_base)
+
 class CMQLogger:
     @staticmethod
     def get_logger(log_name="CMQ_python_sdk", log_file="CMQ_python_sdk.log", log_level=logging.INFO):
